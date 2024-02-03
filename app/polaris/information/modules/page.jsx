7838aa1c7278578c4944f3ee61dee0fdc0658444
Aertic <63727339+Aertic@users.noreply.github.com>
2024-02-03 13:22:05 +0100

    Replace class with className

--- app/polaris/information/modules/page.jsx
+++ app/polaris/information/modules/page.jsx
@@ -4,9 +4,9 @@ export default function Page() {
     return (
         <div className='mainsection'>
             <h6 className='directory'>/</h6>
-            <div class='infonote note'>
-                <img src='https://cdn.polarlab.app/src/docs/img/info.png' alt='alt' class='noteicon' />
-                <p class='notetext'>
+            <div className='infonote note'>
+                <img src='https://cdn.polarlab.app/src/docs/img/info.png' alt='alt' className='noteicon' />
+                <p className='notetext'>
                     Please be advised that the section you are currently viewing is undergoing active development. We
                     are working diligently to provide you with comprehensive and detailed information. We apologize for
                     any inconvenience caused by the incomplete state of this section. Thank you for your understanding
