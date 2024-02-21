728bb59a6310715b7fba1544f03a65f83749aa18
Aertic <63727339+Aertic@users.noreply.github.com>
2024-02-21 21:55:33 +0100

    Added whats new section

--- app/polaris/releases/whats-new/page.jsx
+++ app/polaris/releases/whats-new/page.jsx
@@ -1,6 +1,26 @@
 import Link from 'next/link';
+import { JSDOM } from 'jsdom';
+export default async function Page() {
+    const response = await fetch('https://api.github.com/repos/polarlab-app/Polaris-V2/releases/latest');
+    const data = await response.json();
 
-export default function Page() {
+    const lines = data.body.split('\n');
+
+    let subchapters = [];
+    let currentSubSection = null;
+    let currentSubchapter = null;
+
+    for (const line of lines) {
+        if (line.startsWith('### ') && !line.startsWith('####')) {
+            const formattedLine = line.replace(/### /g, '');
+            currentSubchapter = formattedLine;
+            subchapters[currentSubchapter] = [];
+        } else if (line.startsWith('- ') && currentSubchapter) {
+            const formattedList = line.replace(/- /g, '').replace(/[**]/g, '');
+            subchapters[currentSubchapter].push(formattedList);
+        }
+    }
+
     return (
         <>
             <div className='quicknav'>
@@ -11,13 +31,33 @@ export default function Page() {
             </div>
             <div className='mainsection'>
                 <h6 className='directory'>Polaris Docs / Releases / What's New?</h6>
+
                 <h1 className='title'>What's New?</h1>
                 <hr className='divider'></hr>
                 <div className='chaptercontainer' id='latest'>
-                    <h2 className='chapter'>Polaris V2.3 🥧 Pie</h2>
+                    <h2 className='chapter'>Polaris {data.name}</h2>
                     <hr className='chapterdivider'></hr>
-                    <h3 className='subchapter'>✨ New Features</h3>
-                    <hr className='subchapterdivider'></hr>
+                    {/*{Object.entries(subchapters).map(([subchapterTitle, items]) => (
+                        <>
+                            <h3 className='subchapter'>{subchapterTitle}</h3>
+                            <hr className='subchapterdivider'></hr>
+                            <ul className='mainlist'>
+                                {items.map((item, index) => (
+                                    <li key={index} className='strong'>
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    ))}*/}
+                    {subchapters.map((subchapter) => (
+                        <>
+                            <h3 className='subchapter' key={subchapter}>
+                                {subchapter}
+                            </h3>
+                            <hr className='subchapterdivider' key={subchapter + 'abc'}></hr>
+                        </>
+                    ))}
                 </div>
 
                 <div className='buttoncontainer'>
