61609c96c6eb99ab72416f7361344b5972cd9bf6
Aertic <63727339+Aertic@users.noreply.github.com>
2024-02-23 22:15:49 +0100

    Added emoji, server and messsage logs

--- app/polaris/logs/message-logs/page.jsx
+++ app/polaris/logs/message-logs/page.jsx
@@ -19,17 +19,56 @@ export default function Page() {
                 <h6 className='directory'>Polaris Docs / Logs / Channel Logs</h6>
                 <h1 className='title'>Channel Logs</h1>
                 <hr className='divider'></hr>
+                <div className='note warningnote'>
+                    <img className='noteicon' src='https://cdn.polarlab.app/src/docs/img/warning.png'></img>
+                    <p className='maintext'>
+                        Message logs cannot be used to spy on people, and do not log every sent message. They only log
+                        messages that were deleted by someone else.
+                    </p>
+                </div>
                 <div className='chaptercontainer' id='summary'>
                     <h2 className='chapter'>Overview</h2>
                     <hr className='chapterdivider'></hr>
+                    <p className='maintext'>
+                        Message logs are a useful feature to see deleted messages, that could contain some offensive or
+                        against the rules content.
+                    </p>
                 </div>
                 <div className='chaptercontainer' id='data-logged'>
                     <h2 className='chapter'>Data Logged</h2>
                     <hr className='chapterdivider'></hr>
+                    <p className='maintext'>Polaris logs the following data a message is deleted:</p>
+                    <p className='maintext'>
+                        <ol className='mainlist'>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>Message Author ID</span>
+                            </li>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>Message Content</span>
+                            </li>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>Action Author</span>
+                            </li>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>Time & Date</span>
+                            </li>
+                        </ol>
+                    </p>
                 </div>
                 <div className='chaptercontainer' id='supported-events'>
                     <h2 className='chapter'>Supported Events</h2>
                     <hr className='chapterdivider'></hr>
+                    <p className='maintext'>As of Polaris V2.4 🥬 Lettuce, all events are officially supported:</p>
+                    <p className='maintext'>
+                        <ol className='mainlist'>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>messageCreate (DEV ONLY)</span>
+                            </li>
+                            <li className='mainlistitem'>
+                                <span className='codeblock'>messageDelete</span>
+                            </li>
+                        </ol>
+                    </p>
                 </div>
 
                 <div className='buttoncontainer'>
