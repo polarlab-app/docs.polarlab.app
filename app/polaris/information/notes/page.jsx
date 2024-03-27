2bba16c8c82c14d9085448d7a3d8b06234e604d6
Aertic <63727339+Aertic@users.noreply.github.com>
2024-03-27 22:26:55 +0100

    Added note text

--- app/polaris/information/notes/page.jsx
+++ app/polaris/information/notes/page.jsx
@@ -29,38 +29,38 @@ export default function Page() {
             <div className='chaptercontainer' id='note-types'>
                 <h2 className='chapter'>Types of notes</h2>
                 <hr className='chapterdivider'></hr>
-                <div className='infonote note'>
-                    <img src='https://cdn.polarlab.app/src/docs/img/info.png' alt='alt' className='noteicon' />
-                    <p className='notetext'>
-                        This is an information note, its purpose is to provide you with information that may not be
-                        obvious to distinguish from the docs, or newly found out content. This note may also say when
-                        something is under development, or general notes about something.
-                    </p>
-                </div>
-                <div className='warningnote note'>
-                    <img src='https://cdn.polarlab.app/src/docs/img/warning.png' alt='alt' className='noteicon' />
-                    <p className='notetext'>
-                        This is a so called warning note. This notes primary purpose is to warn you about compatibility
-                        issues, issues within your configuration or critical information that you should know.
-                    </p>
-                </div>
-                <div className='minornote note'>
-                    <img src='https://cdn.polarlab.app/src/docs/img/minor.png' alt='alt' className='noteicon' />
-                    <p className='notetext'>
-                        This is a minor warning note/ . This notes purpose is to give warn you about very unlikely
-                        issues or just to let you acknowledge something of minor importance.
-                    </p>
-                </div>
-                <div className='correctnote note'>
-                    <img src='https://cdn.polarlab.app/src/docs/img/correct.png' alt='alt' className='noteicon' />
-                    <p className='notetext'>
-                        This is a correct/check note. This note provides helpful tips to assist you, or describes a
-                        specific method to achieve optimal overall results. This note can also congratulate you on your
-                        amazing work with configuration!
-                    </p>
-                </div>
             </div>
-            
+            <div className='infonote note'>
+                <img src='https://cdn.polarlab.app/src/docs/img/info.png' alt='alt' className='noteicon' />
+                <p className='notetext'>
+                    This is an information note, its purpose is to provide you with information that may not be obvious
+                    to distinguish from the docs, or newly found out content. This note may also say when something is
+                    under development, or general notes about something.
+                </p>
+            </div>
+            <div className='warningnote note'>
+                <img src='https://cdn.polarlab.app/src/docs/img/warning.png' alt='alt' className='noteicon' />
+                <p className='notetext'>
+                    This is a so called warning note. This notes primary purpose is to warn you about compatibility
+                    issues, issues within your configuration or critical information that you should know.
+                </p>
+            </div>
+            <div className='minornote note'>
+                <img src='https://cdn.polarlab.app/src/docs/img/minor.png' alt='alt' className='noteicon' />
+                <p className='notetext'>
+                    This is a minor warning note/caution note. This notes purpose is to give warn you about very
+                    unlikely issues or just to let you acknowledge something of minor importance.
+                </p>
+            </div>
+            <div className='correctnote note'>
+                <img src='https://cdn.polarlab.app/src/docs/img/correct.png' alt='alt' className='noteicon' />
+                <p className='notetext'>
+                    This is a correct/check note. This note provides helpful tips to assist you, or describes a specific
+                    method to achieve optimal overall results. This note can also congratulate you on your amazing work
+                    with configuration!
+                </p>
+            </div>
+
             <ButtonContainer />
         </div>
     );
